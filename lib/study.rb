3109db9033bafcc2b727fb469340d8de5c966f69
Alexandr Yamschikov <yamshchikov.as73@gmail.com>
2018-08-28 14:25:14 +0400

    4 of 5 tests passes

--- lib/study.rb
+++ lib/study.rb
@@ -4,33 +4,18 @@ require 'pry'
 module Study
   class Config
     def initialize(&block)
-      @data = {}
       instance_eval(&block)
     end
     
     def method_missing(name, *args, &block)
-      puts '===== name ====='
-      puts name.to_s + " called"
-      puts '===== args ====='
-      puts args
-      puts '===== block ====='
-      puts block
-      puts '===== block_given? ====='
-      puts block_given?
-      puts '===== ret args0 ====='
-      args[0]
-      puts '===== data ====='
-      puts @data
-    end
-
-    def fff
-
+      instance_variable_set("@#{name}".to_sym, args[0] || Config.build(&block))
+      self.singleton_class.send(:define_method, name, proc { instance_variable_get("@#{name}")})
+      puts !args[0] && !block_given? && !instance_variable_defined("@#{name}")
     end
 
     def self.build(&block)
       raise ArgumentError if !block_given?
       Config.new(&block)
     end
-
   end
 end
