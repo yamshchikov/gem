01b4350c0ac931d43bf4832bd9aec7590f4b6b45
Alexandr Yamschikov <yamshchikov.as73@gmail.com>
2018-08-28 14:27:05 +0400

    Delete excessive puts

--- lib/study.rb
+++ lib/study.rb
@@ -10,7 +10,6 @@ module Study
     def method_missing(name, *args, &block)
       instance_variable_set("@#{name}".to_sym, args[0] || Config.build(&block))
       self.singleton_class.send(:define_method, name, proc { instance_variable_get("@#{name}")})
-      puts !args[0] && !block_given? && !instance_variable_defined("@#{name}")
     end
 
     def self.build(&block)
