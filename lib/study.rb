03f9192eb69bac9ec779b4fce3ac405cf6a64a58
Alexandr Yamschikov <yamshchikov.as73@gmail.com>
2018-08-28 12:39:43 +0400

    Puts name, args, block for method_missing

--- lib/study.rb
+++ lib/study.rb
@@ -3,14 +3,33 @@ require 'pry'
 
 module Study
   class Config
-    attr_accessor :value
-    def self.method_missing name, *args, &block #name - имя метода, *args - его аттрибуты, &block - блок кода если есть.
+    def initialize(&block)
+      @data = {}
+      instance_eval(&block)
+    end
+    
+    def method_missing(name, *args, &block)
+      puts '===== name ====='
       puts name.to_s + " called"
+      puts '===== args ====='
+      puts args
+      puts '===== block ====='
+      puts block
+      puts '===== block_given? ====='
+      puts block_given?
+      puts '===== ret args0 ====='
+      args[0]
+      puts '===== data ====='
+      puts @data
     end
 
-    def self.build &block
+    def fff
+
+    end
+
+    def self.build(&block)
       raise ArgumentError if !block_given?
-      yield block
+      Config.new(&block)
     end
 
   end
